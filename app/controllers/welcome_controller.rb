c31aa8b82db28d902f31f38a7d8d2c2e035a8174
MisShellyMac <michellemccarthy@hotmail.com>
2015-09-08 10:15:54 -0700

    WIP main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @snapple_facts = SnappleFact.all
+    render :index
+  end
+
+  def random
+    # @random = SnappleFact.sample(10)
+    @snapple_facts = SnappleFact.first
   end
 end
